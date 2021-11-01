d4aafb6618ffed58200d61fbd4d2749f35f37db8
valeria-trossero <trosserovaleria@gmail.com>
2021-11-01 18:40:25 -0300

    Update LogIn.vue

--- src/components/LogIn.vue
+++ src/components/LogIn.vue
@@ -6,7 +6,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login Page</title>
+  <title>Login Page - </title>
 
   &lt;!&ndash;Bootsrap 4 CDN&ndash;&gt;
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
