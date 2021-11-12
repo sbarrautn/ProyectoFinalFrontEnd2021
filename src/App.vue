9eb0c719fd7b95d2be8d81ae1ef5b8740d5984be
emiliano buscarol <emi_310397@hotmail.com.ar>
2021-11-12 15:30:48 -0300

    Integrated Creative Tim Vue template.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,8 @@
 <template>
-  <div id="app">
-    <img src="">  <!-- IMAGEN DE EN EL CENTRO DE LA PÁGINA -->
-    <router-view/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
 export default {
-  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
