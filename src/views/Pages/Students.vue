6f90317d62105a7e525d262616d3f1f52e9ab501
emiliano buscarol <emi_310397@hotmail.com.ar>
2021-11-29 18:31:42 -0300

    (Wip) Creating scaffolding for Students page.

---
+++ src/views/Pages/Students.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center profile-header"
+         style="min-height: 600px; background-image: url(img/theme/students-in-classroom-792x418.jpg); background-size: cover; background-position: center top;">
+      <b-container fluid>
+        <!-- Mask -->
+        <span class="mask bg-gradient-success opacity-8"></span>
+        <!-- Header container -->
+        <b-container fluid class="d-flex align-items-center">
+          <b-row >
+            <b-col lg="7" md="10">
+              <h1 class="display-2 text-white">Gestión de estudiantes</h1>
+              <p class="text-white mt-0 mb-5">Aquí puedes llevar el control de tus estudiantes</p>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-container>
+    </div>
+
+    <b-container fluid class="mt--6">
+      <b-row>
+        <StudentsTable></StudentsTable>
+        <b-col xl="8" class="order-xl-1">
+          <new-student></new-student>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import router from "../../routes/router";
+import NewStudent from './Students/NewStudent.vue';
+import StudentsTable from './Students/StudentsTable.vue';
+
+export default {
+  data() {
+    return {
+      error: '',
+      model: {
+        email: '',
+      }
+    };
+  },
+  components: {
+    NewStudent,
+    StudentsTable
+  },
+  methods: {
+    onSubmit() {
+    }
+  },
+  beforeCreate() {
+    let session = localStorage.getItem('session');
+    if (!session) {
+      session = sessionStorage.getItem('session');
+    }
+    if (!session) {
+      router.push({ name: 'login' });
+    }
+  }
+};
+</script>
---
+++ src/views/Pages/Students/StudentsTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <b-card no-body class="bg-default shadow">
+    <b-card-header class="bg-transparent border-0">
+      <h3 class="mb-0 text-white">Estudiantes</h3>
+    </b-card-header>
+
+    <el-table class="table-responsive table table-dark"
+              header-row-class-name="thead-dark"
+              :data="students">
+      <el-table-column label="Id"
+                       min-width="310px"
+                       prop="id">
+        <template v-slot="{row}">
+          <b-media no-body class="align-items-center">
+            <a href="#" class="mr-3">
+              <b-img class="avatar" rounded="circle" alt="Image placeholder" :src="row.img" />
+            </a>
+            <b-media-body>
+              <span class="font-weight-600 name mb-0 text-sm">{{row.title}}</span>
+            </b-media-body>
+          </b-media>
+        </template>
+      </el-table-column>
+      <el-table-column label="Nombre"
+                       prop="firstName"
+                       min-width="140px">
+      </el-table-column>
+      <el-table-column label="Apellido"
+                       prop="lastName"
+                       min-width="140px">
+      </el-table-column>
+      <el-table-column label="Email"
+                       prop="email"
+                       min-width="140px">
+      </el-table-column>
+
+      <el-table-column label="Estado"
+                       min-width="170px"
+                       prop="status">
+        <template v-slot="{row}">
+          <badge class="badge-dot mr-4">
+            <i :class="`bg-${row.statusType}`"></i>
+            <span class="status" :class="`text-${row.statusType}`">{{row.status}}</span>
+          </badge>
+        </template>
+      </el-table-column>
+    </el-table>
+
+  </b-card>
+</template>
+<script>
+import projects from './../../Tables/projects'
+import students from './../../Tables/studentsData'
+import { Table, TableColumn} from 'element-ui'
+export default {
+  name: 'light-table',
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn
+  },
+  data() {
+    return {
+      projects,
+      students,
+      currentPage: 1
+    };
+  }
+}
+</script>
