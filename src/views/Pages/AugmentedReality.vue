9e26e44ed61840741d924e46972fc907378e4a12
Santiago Barra <sbarra@sanfrancisco.utn.edu.ar>
2022-01-10 10:42:33 -0300

    Agregado de funciones de AR solo falta algo de maquetacion

---
+++ src/views/Pages/AugmentedReality.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <!-- Header -->
+    <div class="header bg-gradient-success py-7 py-lg-8 pt-lg-9">
+      <b-container fluid>
+        <b-row class="justify-content-xl-center" align-v="center">
+        
+            <iframe src="https://wvlhk.csb.app/" width="300"
+                height="500" allow="camera; microphone" 
+                style="
+                  position: absolute;
+                  top: 10px;
+                  width: 50%;
+                  text-align: left;
+                  z-index: 1;
+                ">
+            </iframe>
+        
+        </b-row>
+      </b-container>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
+             xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+    <!-- Page content -->
+    <b-container class="mt--8 pb-5">
+      <b-row class="justify-content-center">
+        <b-col lg="5" md="7">
+          
+            
+
+          
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+import router from "../../routes/router";
+
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+
+<style>
+   
+  </style>
