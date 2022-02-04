b0838e5be3df23d95530bc8b7191bee68737e1f7
Santiago Barra <sbarra@sanfrancisco.utn.edu.ar>
2022-02-04 18:20:29 -0300

    assignament

--- src/views/Pages/Tasks/AssignmentsTask.vue
+++ src/views/Pages/Tasks/AssignmentsTask.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center profile-header"
+    <div class="header pb-0 pt-5 pt-lg-8 d-flex align-items-center profile-header"
          style="min-height: 600px; background-image: url(img/theme/students-in-classroom-792x418.jpg); background-size: cover; background-position: center top;">
       <b-container fluid>
         <!-- Mask -->
@@ -10,56 +10,63 @@
           <b-row>
             <b-col lg="9" md="12">
               <h1 class="display-3 text-white">Nueva asignación de tarea</h1>
-              <p class="text-white mt-0 mb-5">Aquí puede realizar nuevas asignaciones de tareas pendientes</p>
+              <p class="text-white mt-0 mb-0">Aquí puede realizar nuevas asignaciones de tareas pendientes</p>
             </b-col>
           </b-row>
         </b-container>
       </b-container>
     </div>
     <!-- Page content -->
-    <b-container class="mt--4 pb-5">
-        
+    <b-container class="mt -0 pb-5">
+                <div class="card-group mt-0">
+                  <card
+                    title="Tarea titulo"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                  >
+                  <b-button href="#" variant="primary">Go somewhere</b-button>
+                  </card>
+                </div>
     </b-container>
   </div>
 </template>
 <script>
 import router from "../../../routes/router";
+import axios from "axios";
 
+//import axios from "axios";
 export default {
+
+
+
   data() {
     return {
       error: '',
       model: {
-        email: '',
-        password: '',
-        rememberMe: false
+        id: '',
+        title: '',
+        status:''
       }
     };
   },
-  methods: {
-    onSubmit() {
-      axios.post('http://api.proyecto.test/api/asignaciones', {
-        email: this.model.email,
-        password: this.model.password
-      })
-        .then((response) => {
-          const sessionHash = response.data.data.hash;
-          if (this.model.rememberMe) {
-            localStorage.setItem('session', sessionHash);
-          } else {
-            sessionStorage.setItem('session', sessionHash);
-          }
+    created() {
+                let tareas = {
+                           id: '',
+                           title:'',
 
-          router.push({name: 'dashboard'});
-        })
-        .catch((error) => {
-          if (error.response.data.errors.message) {
-            this.$notify({type: 'warning', verticalAlign: 'bottom', horizontalAlign: 'center', message: error.response.data.errors.message});
-          } else {
-            this.$notify({type: 'danger', verticalAlign: 'bottom', horizontalAlign: 'center', message: error.message});
-          }
-        });
-    }
-  }
+                        };
+                axios.post('http://api.proyecto.test/api/tasks', {
+                                                                     
+                                                                  })
+                                                                  .then((response)=> {
+                                                                    console.log("por then");
+                                                                    console.log(response.data);
+                                                                  })
+                                                                  .catch((error)=> {
+                                                                    console.log("por catch");
+                                                                    console.log(error);
+                                                                  });
+    
+  },
 };
 </script>
