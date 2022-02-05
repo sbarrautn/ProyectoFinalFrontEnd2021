442b75169becabbed9dba38e4fff3ab20e224612
Santiago Barra <sbarra@sanfrancisco.utn.edu.ar>
2022-02-05 08:59:49 -0300

    actualizacion

--- src/views/Pages/AugmentedReality.vue
+++ src/views/Pages/AugmentedReality.vue
@@ -2,11 +2,6 @@
   <div>
     <!-- Header -->
     <div class="header bg-gradient-success py-7 py-lg-8 pt-lg-9">
-      <b-container fluid>
-        <b-row class="justify-content-xl-center" align-v="center">
-           
-        </b-row>
-      </b-container>
       <div class="separator separator-bottom separator-skew zindex-100">
         <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
              xmlns="http://www.w3.org/2000/svg">
@@ -15,19 +10,12 @@
       </div>
     </div>
     <!-- Page content -->
-    <b-container class="mt--8 pb-5">
-      <b-row class="justify-content-xl-center" align-v="center" style="margin top: 100px">
-            <iframe src="https://wvlhk.csb.app/" width="300"
-                height="500" allow="camera; microphone" 
-                style="
-                  position: absolute;
-                  top: 10px;
-                  width: 50%;
-                  text-align: left;
-                  z-index: 1;
-                ">
+    <b-container class="mt--1 pb-5" align-v="center">
+     
+           <iframe align-v="center" src="https://wvlhk.csb.app/" width="300"
+                height="500" allow="camera; microphone">
             </iframe>
-        </b-row>
+
     </b-container>
   </div>
 </template>
